a869751219c79a65d1de582ac33f461e95b83f6a
cbsobral <60544376+cbsobral@users.noreply.github.com>
2020-11-16 17:31:10 +0100

    Add files via upload

---
+++ functions.R
@@ -0,0 +1,21 @@
+# append senator main df
+append_df <- function(sen, speeches_df){
+  # rename and filter columns
+  sen <- sen %>% 
+    rename(
+      CodigoPronunciamento = docvar1,
+      Pronunciamento = text) %>% 
+    select(CodigoPronunciamento, Pronunciamento)
+  
+  # merge with main df and delete empty rows
+  sen_df <- merge(sen, speeches_df, by = "CodigoPronunciamento") %>% 
+        na_if("") %>%
+        na.omit
+  
+     return(sen_df)
+}
+
+
+
+
+senators <- append_df(sen = sen, speeches_df = speeches_df)
