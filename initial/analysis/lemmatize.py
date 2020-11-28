9937e024ac6260ef067869b295d39b25ef4b2f0a
cbsobral <60544376+cbsobral@users.noreply.github.com>
2020-11-28 17:16:32 +0100

    Update

---
+++ initial/analysis/lemmatize.py
@@ -0,0 +1,24 @@
+import spacy
+import pandas as pd
+nlp = spacy.load('pt_core_news_sm')
+
+# Import .csv
+senado = pd.read_csv('C:/Users/carol/Desktop/sen.csv', encoding = "LATIN1")
+
+def lemmatize(text):
+    """Perform lemmatization in text
+       Returns lower case lemmatized texts
+    """
+    doc = nlp(text)
+    lemma_text = ' '.join([str(tok.lemma_).lower() for tok in doc
+                  if tok.is_alpha and tok.text.lower()])
+    return lemma_text
+
+# Apply function
+senado['clean'] = senado['Pronunciamento'].apply(lemmatize)
+
+# Select columns
+senado1 = senado[["CodigoPronunciamento", "clean"]]
+
+# Save .csv
+senado1.to_csv('senado1.csv')
